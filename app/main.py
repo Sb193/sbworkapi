eb27086f2ecf64ebd2b0c99e3b92936643d7a0f9
Sb193 <103234991+Sb193@users.noreply.github.com>
2025-05-05 07:47:26 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,7 +3,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.routers import  user, recruiter, job, auth
 from app.db.database import engine
 from app.models import user as user_model, job as job_model
-from app.core.elasticsearch import init_elasticsearch, close_elasticsearch
 from app.core.config import settings
 import uvicorn
 
@@ -29,13 +28,7 @@ app.include_router(user.router)
 app.include_router(recruiter.router)
 app.include_router(job.router)
 
-@app.on_event("startup")
-async def startup_event():
-    await init_elasticsearch()
 
-@app.on_event("shutdown")
-async def shutdown_event():
-    await close_elasticsearch()
 
 if __name__ == "__main__":
     uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
